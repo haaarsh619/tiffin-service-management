59bfda6492bdd04b1b5269df3ad3dc49e9a1abf8
Harsh <harsh.acharya2001@gmail.com>
2025-05-04 14:02:05 +0530

    feat: typeorm and sqllite setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'sqlite',
+      database: configService.get<string>('DATABASE_PATH'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], 
+      synchronize: true,
+    }),
+    inject: [ConfigService],
+  }), ConfigModule.forRoot({
+    isGlobal:true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tiffin Service APIs')
+    .setDescription('CRUD')
+    .setVersion('1.0')
+    .addTag('services')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
